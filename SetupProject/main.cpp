fdb3108721314fcb2e11eeb2ce75ca50abf47a5d
andrew1593571 <andrewkeller2002@gmail.com>
2025-08-26 16:51:36 -0600

    Add Spaces to Math string

--- SetupProject/main.cpp
+++ SetupProject/main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     std::cout << "Andrew Keller" << std::endl;
-    std::cout << 0 + 1 << "Math" << 0 - 1 << std::endl;
+    std::cout << 0 + 1 << " Math " << 0 - 1 << std::endl;
     std::cout << "This is Line 2" << std::endl;
     std::cout << "This is Line 3" << std::endl;
     std::cout << "This is Line 4" << std::endl;
