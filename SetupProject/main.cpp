acbdd57e931f7a3ce7b0a5759d0364b34a18eddb
andrew1593571 <andrewkeller2002@gmail.com>
2025-08-26 16:50:13 -0600

    Complete SetupProject to assignment requirements

--- SetupProject/main.cpp
+++ SetupProject/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Andrew Keller" << std::endl;
+    std::cout << 0 + 1 << "Math" << 0 - 1 << std::endl;
+    std::cout << "This is Line 2" << std::endl;
+    std::cout << "This is Line 3" << std::endl;
+    std::cout << "This is Line 4" << std::endl;
+    std::cout << "This is Line 5" << std::endl;
     return 0;
 }
